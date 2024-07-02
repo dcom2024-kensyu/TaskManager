3d9c4ba590ac8e646140c5adf53e10e8fb5b5cd8
楓 石守 <fu.ishimori@dcom-web.co.jp>
2024-07-02 16:22:52 +0900

    //devブランチ

--- TaskManager.Web/Program.cs
+++ TaskManager.Web/Program.cs
@@ -8,6 +8,7 @@ namespace TaskManager.Web
     {
         public static void Main(string[] args)
         {
+            //devƒuƒ‰ƒ“ƒ`
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
