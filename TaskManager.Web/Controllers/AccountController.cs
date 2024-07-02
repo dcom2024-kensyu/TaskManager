fb98dc71553c523271e359ef185f2225611e05fa
小玉 拓矢 <takuya.kodama@dcom-web.co.jp>
2024-07-02 15:24:10 +0900

    feat:ログイン画面新規作成

---
+++ TaskManager.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string email, string password)
+        {
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                ModelState.AddModelError(string.Empty, "ログイン情報に誤りがあります。");
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
--- TaskManager.Web/Program.cs
+++ TaskManager.Web/Program.cs
@@ -31,7 +31,7 @@ namespace TaskManager.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Login}/{id?}");
 
             app.Run();
         }
