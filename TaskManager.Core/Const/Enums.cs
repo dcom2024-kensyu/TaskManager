6c1a965f81a539b9411f89ac6c484112f65d546c
小玉 拓矢 <takuya.kodama@dcom-web.co.jp>
2024-07-02 15:49:25 +0900

    feat:不要なファイルを削除

---
+++ TaskManager.Core/Const/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Core.Const
+{
+    public class Enums
+    {
+        public enum UserRole
+        {
+            Administrator = 1,
+            User = 2,
+        }
+    }
+}
